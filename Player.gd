6efc6b790fee6d9a06f185dad816a8f12c4055bc
AmberGehan <74631886+PatrickGehan@users.noreply.github.com>
2022-11-11 13:42:15 -0700

    Changed the weapon to rotate to face the player's cursor on left click

--- Player.gd
+++ Player.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+signal charge_weapon
 signal hit
 
 
@@ -20,12 +21,16 @@ func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		emit_signal("charge_weapon")
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		emit_signal("charge_weapon")
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		emit_signal("charge_weapon")
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		emit_signal("charge_weapon")
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
--- Weapon.gd
+++ Weapon.gd
@@ -1,20 +1,23 @@
 extends Area2D
+signal weapon_swing
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var weapon_charge = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("left_click"):
+	if Input.is_action_just_pressed("left_click"):
+		scale = Vector2(weapon_charge, weapon_charge)
+		weapon_charge = 0
+		emit_signal("weapon_swing")
+		rotation = find_weapon_rotation()
 		#print("Left Click")
+		show()
 		var hit_things
 		hit_things = get_overlapping_bodies()
 		for thing in hit_things:
@@ -22,3 +25,22 @@ func _process(delta):
 			if thing.has_method("die"):
 				#print("Got enemy")
 				thing.die()
+		yield(get_tree().create_timer(1.0), "timeout")
+		hide()
+		
+
+func find_weapon_rotation():
+	var screenCenter = get_viewport_rect().size/2
+	var weaponVector = Vector2(get_viewport().get_mouse_position() - screenCenter)
+	#print("Mouse Position:")
+	#print(get_viewport().get_mouse_position())
+	#print("Screen Center:")
+	#print(screenCenter)
+	#print("Weapon Vector:")
+	#print(weaponVector.normalized())
+	return weaponVector.angle() + PI/2
+	
+
+
+func _on_Player_charge_weapon():
+	weapon_charge += .05
